d7235a9ea7aed3698560355d1d85675925eb1261
ilyas123ahmed <80588277+ilyas123ahmed@users.noreply.github.com>
2022-10-14 17:43:31 +0500

    Create Hello.py

---
+++ Month_02/Week_02/TKinter/Hello.py
@@ -0,0 +1,13 @@
+```python
+from tkinter import *
+
+root = Tk()
+
+# Creating a Label Widget
+myLabel = Label(root, text="Hello World!")
+# Shoving it onto the screen
+myLabel.pack()
+
+
+root.mainloop()
+```
